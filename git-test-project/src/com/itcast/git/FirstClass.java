7449b267067718c24864038052cc7467993598fe
tlmonkey <1017160938@qq.com>
2019-10-19 15:58:25 +0800

    修改FirstClass1

--- git-test-project/src/com/itcast/git/FirstClass.java
+++ git-test-project/src/com/itcast/git/FirstClass.java
@@ -2,18 +2,8 @@ package com.itcast.git;
 
 public class FirstClass {
     public static void main(String args[]){
-        System.out.println("hello git project");
-        System.out.println("hello git project");
-        System.out.println("hello git project");
-        System.out.println("hello git project");
-        System.out.println("hello git project");
-        System.out.println("hello git project");
+
         System.out.println("hello git project");
     }
 
-    public void test(){
-        int i = 0;
-        i += 10;
-        System.out.println(i);
-    }
 }
